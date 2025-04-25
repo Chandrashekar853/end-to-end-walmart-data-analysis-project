a3aaac2250492816682500d2cd0b60952becfc44
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2025-04-25 23:35:06 +0530

    Initial commit

---
+++ SQL query.sql
@@ -0,0 +1,142 @@
+-- Walmart Project Queries
+
+SELECT * FROM walmart;
+
+-- DROP TABLE walmart;
+
+-- DROP TABLE walmart;
+
+-- 
+SELECT COUNT(*) FROM walmart;
+
+SELECT 
+	 payment_method,
+	 COUNT(*)
+FROM walmart
+GROUP BY payment_method;
+
+SELECT 
+	COUNT(DISTINCT branch) 
+FROM walmart;
+
+SELECT MIN(quantity) FROM walmart;
+
+-- Business Problems
+-- Q1 Find different payment method and number of transactions, number of qty sold
+
+
+SELECT 
+	 payment_method,
+	 COUNT(*) as no_payments,
+	 SUM(quantity) as no_qty_sold
+FROM walmart
+GROUP BY payment_method;
+
+
+-- Project Question #2
+-- Identify the highest-rated category in each branch, displaying the branch, category
+-- AVG RATING
+
+SELECT branch, category, avg_rating
+FROM (
+    SELECT 
+        branch,
+        category,
+        AVG(rating) AS avg_rating,
+        RANK() OVER (PARTITION BY branch ORDER BY AVG(rating) DESC) AS rnk
+    FROM walmart
+    GROUP BY branch, category
+) AS ranked
+WHERE rnk = 1;
+
+
+
+
+-- Q.3 Identify the busiest day for each branch based on the number of transactions
+
+SELECT * 
+FROM
+	(SELECT 
+		branch,
+		DAYNAME(STR_TO_DATE(date, '%d/%m/%Y')) as day_name,
+		COUNT(*) as no_transactions,
+		RANK() OVER(PARTITION BY branch ORDER BY COUNT(*) DESC) as rnk
+	FROM walmart
+	GROUP BY branch, day_name
+	) as ranked
+WHERE rnk = 1;
+
+-- Q. 4 
+-- Calculate the total quantity of items sold per payment method. List payment_method and total_quantity.
+
+
+
+SELECT 
+	 payment_method,
+	 -- COUNT(*) as no_payments,
+	 SUM(quantity) as no_qty_sold
+FROM walmart
+GROUP BY payment_method;
+
+
+-- Q.5
+-- Determine the average, minimum, and maximum rating of category for each city. 
+-- List the city, average_rating, min_rating, and max_rating.
+
+SELECT 
+	city,
+	category,
+	MIN(rating) as min_rating,
+	MAX(rating) as max_rating,
+	AVG(rating) as avg_rating
+FROM walmart
+GROUP BY city, category;
+
+
+-- Q.6
+-- Calculate the total profit for each category by considering total_profit as
+-- (unit_price * quantity * profit_margin). 
+-- List category and total_profit, ordered from highest to lowest profit.
+
+SELECT 
+	category,
+	SUM(total) as total_revenue,
+	SUM(total * profit_margin) as profit
+FROM walmart
+GROUP BY 1;
+
+
+-- Q.7
+-- Determine the most common payment method for each Branch. 
+-- Display Branch and the preferred_payment_method.
+
+WITH cte 
+AS
+(SELECT 
+	branch,
+	payment_method,
+	COUNT(*) as total_trans,
+	RANK() OVER(PARTITION BY branch ORDER BY COUNT(*) DESC) as rnk
+FROM walmart
+GROUP BY branch, payment_method
+)
+SELECT *
+FROM cte
+WHERE rnk = 1;
+
+
+-- Q.8
+-- Categorize sales into 3 group MORNING, AFTERNOON, EVENING 
+-- Find out each of the shift and number of invoices
+
+SELECT
+	branch,
+CASE 
+		WHEN HOUR(TIME(time)) < 12 THEN 'Morning'
+		WHEN HOUR(TIME(time)) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+	END day_time,
+	COUNT(*)
+FROM walmart
+GROUP BY branch, 2
+ORDER BY 1, 3 DESC
